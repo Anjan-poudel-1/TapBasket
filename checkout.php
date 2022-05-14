a030f4e7bf0018837b53559e420b860e71bef2ad
Anjan_poudel <poudelanjan8@gmail.com>
2022-05-15 00:19:31 +0545

    Paypal integrated

--- checkout.php
+++ checkout.php
@@ -1,15 +1,14 @@
 <?php
 SESSION_START();
 // session_destroy();
-if(!(isset($_SESSION['isAuthenticated']) && $_SESSION['isAuthenticated']===true) ){
+if (!(isset($_SESSION['isAuthenticated']) && $_SESSION['isAuthenticated'] === true)) {
 
-    header ('location:index.php');
+    header('location:index.php');
 }
 
-if(!(isset($_SESSION['cart']))){
+if (!(isset($_SESSION['cart']))) {
 
-    $_SESSION['cart']= [];
-
+    $_SESSION['cart'] = [];
 }
 if (!(isset($_SESSION['user_id']))) {
 
@@ -31,41 +30,39 @@ $finalPayment = false;
 
 
 //For checkout form 
-if(isset($_POST['submitPickDetails'])){
+if (isset($_POST['submitPickDetails'])) {
 
-    $selectedDateError ='';
-    $selectedTimeError ='';
+    $selectedDateError = '';
+    $selectedTimeError = '';
     $selectedDate = '';
     $selectedTime = '';
-    $isStepOneCompleted=false;
-    $errCount=0;
+    $isStepOneCompleted = false;
+    $errCount = 0;
 
-    if(isset($_POST['dateSelected']) && $_POST['dateSelected']){
+    if (isset($_POST['dateSelected']) && $_POST['dateSelected']) {
         $selectedDate = $_POST['dateSelected'];
-    }
-    else{
-        $errCount=1;
-        $selectedDateError ='Please select the date';
+    } else {
+        $errCount = 1;
+        $selectedDateError = 'Please select the date';
     }
 
-    if(isset($_POST['timing'])){
+    if (isset($_POST['timing'])) {
         $selectedTime = $_POST['timing'];
-    }
-    else{
-        $errCount=1;
-        $selectedTimeError ='Please select the timing';
+    } else {
+        $errCount = 1;
+        $selectedTimeError = 'Please select the timing';
     }
 
-    if( $errCount==0){
-        $isStepOneCompleted=true;
+    if ($errCount == 0) {
+        $isStepOneCompleted = true;
     }
-   echo $selectedDate;
-        echo $selectedTime;
+    echo $selectedDate;
+    echo $selectedTime;
 }
 
 
-if(isset($_POST['goToCart'])){
-    header ('location:cart.php');
+if (isset($_POST['goToCart'])) {
+    header('location:cart.php');
 }
 
 
@@ -73,60 +70,58 @@ if(isset($_POST['goToCart'])){
 <?php
 
 
-        if(isset($_GET['collectionSlot'])&&(isset($_GET['timing'])&& isset($_GET['payment'])&& $_GET['payment'])){
-            $myCart = $_SESSION['cart'];
-        $selectedDate=$_GET['collectionSlot'];
-        $tempData =  explode(",",$selectedDate)[0];
-        $tempDay =  explode(",",$selectedDate)[1];
-        $year = date("Y");
-        $selectedTime=$_GET['timing'];
-        $paymentstatus="false";
-        $selectedDate=$tempData.", ".$year; 
+if (isset($_GET['collectionSlot']) && (isset($_GET['timing']) && isset($_GET['payment']) && $_GET['payment'])) {
+    $myCart = $_SESSION['cart'];
+    $selectedDate = $_GET['collectionSlot'];
+    $tempData =  explode(",", $selectedDate)[0];
+    $tempDay =  explode(",", $selectedDate)[1];
+    $year = date("Y");
+    $selectedTime = $_GET['timing'];
+    $paymentstatus = "false";
+    $selectedDate = $tempData . ", " . $year;
+    $subtotal = 0;
 
-        $subtotal=0;
-        
-        foreach($myCart as $product_id=>$quantity){
+    foreach ($myCart as $product_id => $quantity) {
 
-            $discountPrice=0;
-            $finalprice=0;
+        $discountPrice = 0;
+        $finalprice = 0;
 
-            $tempProductId=$product_id;
+        $tempProductId = $product_id;
         $sqlDiscount = "SELECT DISCOUNT_RATE FROM DISCOUNT WHERE PRODUCT_ID=$product_id";
-                    $stidDiscount = oci_parse($conn, $sqlDiscount);
-                    oci_execute($stidDiscount);
-        
-        
+        $stidDiscount = oci_parse($conn, $sqlDiscount);
+        oci_execute($stidDiscount);
+
+
         while (oci_fetch($stidDiscount)) {
-               $discountPrice = oci_result($stidDiscount, 'DISCOUNT_RATE');
-                        
+            $discountPrice = oci_result($stidDiscount, 'DISCOUNT_RATE');
         }
         $sqlproductprice = "SELECT PRICE FROM PRODUCT WHERE PRODUCT_ID=$tempProductId";
         $stidProductPrice = oci_parse($conn, $sqlproductprice);
         oci_execute($stidProductPrice);
 
 
-            while (oci_fetch($stidProductPrice )) {
-            $tempPrice = oci_result($stidProductPrice ,'PRICE');        
-            }
-        $finalprice=$tempPrice-$discountPrice;
-        $subtotal+=$finalprice;
+        while (oci_fetch($stidProductPrice)) {
+            $tempPrice = oci_result($stidProductPrice, 'PRICE');
         }
+        $finalprice = ($tempPrice - $discountPrice)*$quantity;
+        $subtotal += $finalprice;
+    }
 
 
-       $insertOrderPlace = "INSERT INTO ORDERPLACE(TIMESLOT,DATE_OF_COLLECTION,DAY,PAYMENT_STATUS,USER_ID,subtotal) VALUES (:selectedTime,:selecteddate,:tempday,:paymentstatus,:userid,:subtotal)";
-       $collectionslotstid = (oci_parse($conn, $insertOrderPlace));
-       oci_bind_by_name($collectionslotstid , ":userid", $user_id);
-       oci_bind_by_name($collectionslotstid , ":subtotal", $subtotal);
-       oci_bind_by_name($collectionslotstid , ":selecteddate", $selectedDate);
-       oci_bind_by_name($collectionslotstid , ":selectedTime", $selectedTime );
-       oci_bind_by_name($collectionslotstid , ":tempday", $tempDay);
-       oci_bind_by_name($collectionslotstid , ":paymentstatus", $paymentstatus);
-       oci_execute($collectionslotstid , OCI_NO_AUTO_COMMIT);
-       oci_commit($conn);
-       oci_free_statement($collectionslotstid);
+    $insertOrderPlace = "INSERT INTO ORDERPLACE(TIMESLOT,DATE_OF_COLLECTION,DAY,PAYMENT_STATUS,USER_ID,subtotal) VALUES (:selectedTime,:selecteddate,:tempday,:paymentstatus,:userid,:subtotal)";
+    $collectionslotstid = (oci_parse($conn, $insertOrderPlace));
+    oci_bind_by_name($collectionslotstid, ":userid", $user_id);
+    oci_bind_by_name($collectionslotstid, ":subtotal", $subtotal);
+    oci_bind_by_name($collectionslotstid, ":selecteddate", $selectedDate);
+    oci_bind_by_name($collectionslotstid, ":selectedTime", $selectedTime);
+    oci_bind_by_name($collectionslotstid, ":tempday", $tempDay);
+    oci_bind_by_name($collectionslotstid, ":paymentstatus", $paymentstatus);
+    oci_execute($collectionslotstid, OCI_NO_AUTO_COMMIT);
+    oci_commit($conn);
+    oci_free_statement($collectionslotstid);
 
-       if($insertOrderPlace){
-        
+    if ($insertOrderPlace) {
+
         $sql = "Select ORDERPLACE_ID from ORDERPLACE where 1=1 order by ORDERPLACE_ID desc";
         $select = oci_parse($conn, $sql);
         oci_execute($select, OCI_NO_AUTO_COMMIT);
@@ -136,27 +131,55 @@ if(isset($_POST['goToCart'])){
             if ($key == "ORDERPLACE_ID")
                 $orderplace_id = $value[0];
         }
-        $subtotal=0;
-        foreach($myCart as $product_id=>$quantity){
-            
-            $orderlistsql="INSERT INTO ORDERLIST(QUANTITY,ORDERPLACE_ID,PRODUCT_ID) VALUES(:quantity,:orderPlaceId,:productId)";
+        
+        foreach ($myCart as $product_id => $quantity) {
+
+            $orderlistsql = "INSERT INTO ORDERLIST(QUANTITY,ORDERPLACE_ID,PRODUCT_ID) VALUES(:quantity,:orderPlaceId,:productId)";
             $orderliststid = (oci_parse($conn, $orderlistsql));
             oci_bind_by_name($orderliststid, ":productId", $product_id);
-            oci_bind_by_name($orderliststid, ":quantity", $quantity );
+            oci_bind_by_name($orderliststid, ":quantity", $quantity);
             oci_bind_by_name($orderliststid, ":orderPlaceId", $orderplace_id);
-            oci_execute($orderliststid , OCI_NO_AUTO_COMMIT);
+            oci_execute($orderliststid, OCI_NO_AUTO_COMMIT);
             oci_commit($conn);
             oci_free_statement($orderliststid);
         }
-        $_SESSION['cart']=[];
+        $_SESSION['cart'] = [];
 
-       }
+        ?>
+             <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" id="paypal-submit" method="post" class="paypal">
+      <!-- Paypal business test account email id so that you can collect the payments. -->
+      <input type="hidden" name="business" value="sb-knnls16326444@business.example.com">
+      <!-- Buy Now button. -->
+      <input type="hidden" name="cmd" value="_xclick">
+      <!-- Details about the item that buyers will purchase. -->
+      <input type="hidden" name="item_name" value="3000">
+      <input type="hidden" name="item_number" value="1000"> 
+      <!-- //user_id -->
+      <input type="hidden" name="amount" value="<?php echo  $subtotal?>">
+      <!-- PUT THE PRICE IN SESSION -->
+      <input type="hidden" name="currency_code" value="GBP">
+      <input type="hidden" name="rm" value="2">
+      <!-- URLs -->
+      <input type='hidden' name='cancel_return' value='http://localhost/TapBasket/index.php'>
+      <input type='hidden' name='return' value='http://localhost/TapBasket/cart.php'>
+      <!-- payment button. -->
+      <input type="image" name="submit" border="0" src="sandbox/paypal.svg" alt="PayPal - The safer, easier way to pay online">
+      <img alt="" border="0" width="1" height="1" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif">
+    </form>       
+        <?php
+        
+        echo '<script>
+        document.getElementById("paypal-submit").submit();
+        </script>';
+        $subtotal = 0;
     }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -164,12 +187,13 @@ if(isset($_POST['goToCart'])){
     <title>Document</title>
     <link rel="stylesheet" href="./assets/styles/index.css">
 </head>
+
 <body data-theme="default" id="get-theme">
-    
+
     <div class="page checkout-page">
 
-        <?php 
-            include './components/navbars/primary-navbar.php';
+        <?php
+        include './components/navbars/primary-navbar.php';
         ?>
 
         <div class="container page__body">
@@ -177,19 +201,19 @@ if(isset($_POST['goToCart'])){
 
                 <div class="section__header__heading">
 
-                  Cart Checkout
+                    Cart Checkout
 
                 </div>
             </div>
             <div class="checkout__body">
 
-               <?php
-               include './components/pages/Checkout/checout-steps.php';
-               ?>
+                <?php
+                include './components/pages/Checkout/checout-steps.php';
+                ?>
 
                 <?php
-               include './components/pages/Checkout/order-details.php';
-               ?>
+                include './components/pages/Checkout/order-details.php';
+                ?>
 
             </div>
         </div>
@@ -198,23 +222,29 @@ if(isset($_POST['goToCart'])){
 
     <!-- Page Footers -->
     <?php
-        include './components/resuables/page-footer.php';
+    include './components/resuables/page-footer.php';
     ?>
 
-     <!-- Copyright  -->
+    <!-- Copyright  -->
 
     <?php
-        include './components/resuables/copyright.php';
+    include './components/resuables/copyright.php';
     ?>
 
     <!--Bottom Nav-->
 
-<?php
-include './components/navbars/bottom-navbar.php';
-?>
-<!-- <script src="https://www.paypal.com/sdk/js?client-id=Aauq_1MoKXDNfcbsXq1LcdFmA3QThxyBEAkB9NJEZ14ukELptkYLjXizi4VJx6kN-AyC2isMNz2Aj1Ug"></script>
-<script>paypal.Buttons().render('body');</script> -->
+    <?php
+    include './components/navbars/bottom-navbar.php';
+    ?>
+    
+
+    
+
+
 </body>
 
 <script src="app.js"></script>
+
+
+
 </html>
