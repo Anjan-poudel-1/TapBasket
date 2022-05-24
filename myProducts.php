0fc70cee44ff248c76331e4578c51ebc1c889f13
Anjan_poudel <poudelanjan8@gmail.com>
2022-05-24 19:00:00 +0545

    Trader navbar

--- myProducts.php
+++ myProducts.php
@@ -18,7 +18,7 @@ include('connection.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My Products</title>
     <link rel="stylesheet" href="./assets/styles/index.css">
 
 </head>
@@ -181,8 +181,8 @@ include('connection.php');
     <!--Bottom Nav-->
 
     <?php
-    include './components/navbars/bottom-navbar.php';
-    ?>
+    // include './components/navbars/bottom-navbar.php';
+    ?> 
 
 </body>
 <script src="app.js">
---
+++ productDiscount.php
@@ -0,0 +1,46 @@
+<?php
+SESSION_START();
+if ((isset($_SESSION['role']) && $_SESSION['role'] == 'customer')) {
+    header('location:index.php');
+}
+
+include('connection.php');
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Products</title>
+    <link rel="stylesheet" href="./assets/styles/index.css">
+
+</head>
+<body data-theme="default" id="get-theme">
+
+    <div class="page home-page">
+
+        <?php
+        include './components/navbars/primary-navbar.php';
+        ?>
+    </div>
+
+    <!-- Page Footers -->
+    <?php
+    include './components/resuables/page-footer.php';
+    ?>
+
+    <!-- Copyright  -->
+
+    <?php
+    include './components/resuables/copyright.php';
+    ?>
+</body>
+<script src="app.js">
+
+</script>
+
+</html>
